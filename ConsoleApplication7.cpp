2ffa23190c8fa7d4d7ef981f6c63ca0bea1241d1
Alexey Nizovtsev <114223854+AlexeyNizovtsev@users.noreply.github.com>
2024-02-03 15:26:23 +0300

    Add files via upload

---
+++ ConsoleApplication7.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+
+void show(int mass[], int n) {
+    std::cout << "Generated array: ";
+
+    for (int i = 0; i < n; i++) {
+        std::cout << mass[i] << " ";
+    }
+    std::cout << std::endl;
+
+}
+
+void sravn(int mass[], int n, int x) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (mass[i] - mass[j] == x) {
+                std::cout << mass[i] << " " << mass[j] << std::endl;
+            }
+        }
+    }
+}
+
+
+int main()
+{
+    std::srand(time(0));
+    int n;
+    std::cout << "Size: ";
+    std::cin >> n;
+    int *mass = new int[n];
+    
+    for (int i = 0; i < n; i++) {
+        mass[i] = rand() % 51 + 1;
+    }
+
+    show(mass, n);
+    
+    int x;
+    std::cout << "Number: ";
+    std::cin >> x;
+
+    sravn(mass, n, x);
+
+    delete[] mass;
+
+}
